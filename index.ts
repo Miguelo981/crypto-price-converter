3a80d51868ef6afd5515dbb09056b5e87cee0dd3
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2024-01-01 18:58:04 +0000

    Add CryptoConverter API export

---
+++ index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * CryptoConverter API
+ *
+ * The version of the CryptoConverter document: 1.0.0
+ * 
+ *
+ * Do not edit the class manually.
+ */
+
+export * from "./api";
